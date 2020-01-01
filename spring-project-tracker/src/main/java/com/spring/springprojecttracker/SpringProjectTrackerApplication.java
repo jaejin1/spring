0972acbaccc74242cd769ad91f5713dc4e199944
jaejin1 <opiximeo@gmail.com>
2020-01-01 13:27:04 +0900

    Add daemon listener

--- spring-project-tracker/src/main/java/com/spring/springprojecttracker/SpringProjectTrackerApplication.java
+++ spring-project-tracker/src/main/java/com/spring/springprojecttracker/SpringProjectTrackerApplication.java
@@ -2,12 +2,17 @@ package com.spring.springprojecttracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringProjectTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringProjectTrackerApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringProjectTrackerApplication.class);
+        application.addListeners(new ApplicationPidFileWriter());
+        application.run(args);
     }
 
 }
---
+++ spring-project-tracker/src/main/java/com/spring/springprojecttracker/listener/DaemonListener.java
@@ -0,0 +1,56 @@
+package com.spring.springprojecttracker.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class DaemonListener implements ServletContextListener, Runnable {
+    private Thread thread;
+    private boolean isShutdown = false;
+
+    private ServletContext sc;
+
+    public void startDaemon() {
+        if (thread == null) {
+            thread = new Thread(this, "Daemon thread for background task");
+
+        }
+
+        if (!thread.isAlive()) {
+            thread.start();
+        }
+    }
+
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+
+        while (currentThread == thread && !this.isShutdown) {
+            try {
+                System.out.println("== DaemonListener is running. ==");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("== DaemonListener end. ==");
+    }
+
+    public void contextInitialized (ServletContextEvent event) {
+        System.out.println("== DaemonListener.contextInitialized has been called. ==");
+        sc = event.getServletContext();
+        startDaemon();
+    }
+
+    public void contextDestroyed (ServletContextEvent event) {
+        System.out.println("== DaemonListener.contextDestroyed has been called. ==");
+        this.isShutdown = true;
+        try {
+            thread.join();
+            thread = null;
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+    }
+}
